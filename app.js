8d77797bba4642b524def4e18cb5943a9533428f
Saket Mundra <saketmundra@Sakets-MacBook-Air.local>
2022-01-28 14:59:49 +0530

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express"); // Access
+
+const app=express();
+
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/public/index.html")
+})
+
+
+let port = process.env.PORT || 5000;
+let server=app.listen(port,()=>{
+    console.log("listening to port " + port);
+});
